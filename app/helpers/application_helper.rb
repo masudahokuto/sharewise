bbce2271e20841249db118a8f25be15659f61ee9
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-09-21 04:01:14 +0000

    gest[fix]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,24 @@
 module ApplicationHelper
   # 今日登録したcontent
   def today_contents
-    Content.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
-           .joins(genre: { title: :category }) # current_userが登録したコンテンツのみ
-           .where(categories: { user_id: current_user.id })
+    if current_user
+      Content.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+             .joins(genre: { title: :category })
+             .where(categories: { user_id: current_user.id })
+    else
+      Content.none
+    end
   end
 
   # 昨日登録したcontent
   def yesterday_contents
-    Content.where(created_at: Time.zone.yesterday.beginning_of_day..Time.zone.yesterday.end_of_day)
-           .joins(genre: { title: :category }) # current_userが登録したコンテンツのみ
-           .where(categories: { user_id: current_user.id })
+    if current_user
+      Content.where(created_at: Time.zone.yesterday.beginning_of_day..Time.zone.yesterday.end_of_day)
+             .joins(genre: { title: :category }) # current_userが登録したコンテンツのみ
+             .where(categories: { user_id: current_user.id })
+    else
+      Content.none
+    end
   end
 
 
