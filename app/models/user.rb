291186b7332e01f1834723272add39b6b86ad749
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-09-27 07:18:44 +0000

    image[fix]

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,8 +54,11 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [150, 150]).processed
+
+    # ここでリサイズ処理を適用
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
+
 
   # ユーザーの最新の投稿を取得
   def recent_post
