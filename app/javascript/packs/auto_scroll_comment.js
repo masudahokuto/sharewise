816418b0afabff2b743f4e6ab1709e8951370a91
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-10-14 16:20:21 +0000

    auto-scroll[add]

--- app/javascript/packs/auto_scroll_comment.js
+++
@@ -1,8 +0,0 @@
-  $(document).on('turbolinks:load', function() {
-  $('.auto-scroll-comment').jscroll({ // 追加したdivのclass名と合わせる
-    contentSelector: '.auto-scroll-comment',
-    nextSelector: '.pagination a[rel=next]', // 次ページリンクのセレクタ
-    loadingHtml: '読み込み中',
-    padding: 10
-    });
-  });
