69eec6bcf6ebb213ba942ea23ffd99b5624b7f3f
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-10-16 03:03:14 +0000

    search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,9 @@ class SearchesController < ApplicationController
 
   def search
     @current_user = current_user
-    @users = User.where('nick_name LIKE ? AND is_active = ?', "%#{params[:query]}%", true).page(params[:page]).per(10)
+    @users = User.where('nick_name LIKE ? AND is_active = ?', "%#{params[:query]}%", true)
+                 .order(created_at: :desc) # 新しい順に並べ替え
+                 .page(params[:page])
+                 .per(10)
   end
 end
