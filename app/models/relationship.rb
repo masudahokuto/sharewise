02a71cd5d1cba5875fecc9acf3371e776978c535
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-10-16 13:07:19 +0000

    notification[fix]

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,14 +3,4 @@ class Relationship < ApplicationRecord
   belongs_to :followed, class_name:"User"
 
   has_one :notification, as: :notificable, dependent: :destroy
-
-  # def create_notification
-
-  #   # 通知の作成
-  #   Notification.create(
-  #     user: followed.user,        # 通知を受け取るユーザー（フォローされたユーザー）
-  #     notificable: self,     # 通知対象（このリレーションシップ）
-  #     read: false             # 新しい通知は未読
-  #   )
-  # end
 end
