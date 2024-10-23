fca440d8e6a6edc20d475e97c194915cabaddf81
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-10-23 01:11:11 +0000

    gest-log_in-scss[fix]

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -21,6 +21,27 @@ class Public::RegistrationsController < Devise::RegistrationsController
     mypage_users_path
   end
 
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      # エラーメッセージをセッションに保存
+      session[:error_messages] = resource.errors.full_messages
+      puts "DEBUG: Error messages saved to session: #{session[:error_messages]}" # 追加して確認
+      redirect_to new_user_registration_path  # new アクションにリダイレクト
+    end
+  end
+
   protected
 
   # サインアップ時に許可するパラメータの設定
